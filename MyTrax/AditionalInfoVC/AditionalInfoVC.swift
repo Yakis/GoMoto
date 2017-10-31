ef8d5ad115fee802c5770b750719eadbb79ff2ec
Mugur Moscaliuc <mugur.moscaliuc@gmail.com>
2017-10-31 18:49:37 +0000

    Username Screen created

--- MyTrax/AditionalInfoVC/AditionalInfoVC.swift
+++ MyTrax/AditionalInfoVC/AditionalInfoVC.swift
@@ -7,98 +7,37 @@
 //
 
 import UIKit
-import CoreLocation
 
-class AditionalInfoVC: UIViewController, CLLocationManagerDelegate {
 
-    @IBOutlet weak var firstName: UILabel!
-    
-    @IBOutlet weak var lastName: UILabel!
-    
-    @IBOutlet weak var email: UILabel!
-    
-    @IBOutlet weak var phoneNumber: UILabel!
-    
-    @IBOutlet weak var userType: UILabel!
-    
-    @IBOutlet weak var firebaseUID: UILabel!
-    
-    @IBOutlet weak var adress: UILabel!
+class AditionalInfoVC: UIViewController {
+
     
     var user: TraxUser!
-    var locationManager:CLLocationManager!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var finishButtonOutlet: UIButton!
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.firstName.text = "First Name: \(user.first_name!)"
-//        self.lastName.text = "Last Name: \(user.last_name!)"
-//        self.email.text = "Email: \(user.email!)"
-//        self.phoneNumber.text = "Contact number: \(user.contact_number!)"
-//        self.userType.text = "User type: \(user.user_type!)"
-//        self.firebaseUID.text = "Firebase UID: \(user.firebase_uid!)"
-        
-    }
-
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        determineMyCurrentLocation()
-    }
-    
-    
-    func determineMyCurrentLocation() {
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.startUpdatingLocation()
-           // locationManager.startUpdatingHeading()
+        guard let userType = user.user_type else {return}
+        finishButtonOutlet.roundCorners()
+        switch userType {
+        case "rider":
+            messageLabel.text = "People want to know who you are when you posting something. Just enter your username and you're in! We recomend your name and  bike No. i.e. <Ben672>"
+        default:
+            messageLabel.text = "People want to know who you are when you posting something. Just enter your username and you're in! We recomend your track name ie. <Wild Tracks>"
         }
     }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error) in
-            if (error != nil) {
-                print("Reverse geocoder failed with error" + (error?.localizedDescription)!)
-                return
-            }
-            
-            if placemarks?.count != 0 {
-                let pm = placemarks![0] as CLPlacemark
-                self.displayLocationInfo(placemark: pm)
-            } else {
-                print("Problem with the data received from geocoder")
-            }
-        })
-        
-        
-        let userLocation:CLLocation = locations[0] as CLLocation
-        
-         manager.stopUpdatingLocation()
-        
-        print("user latitude = \(userLocation.coordinate.latitude)")
-        print("user longitude = \(userLocation.coordinate.longitude)")
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
-    {
-        print("Error \(error)")
-    }
-    
 
     
-    func displayLocationInfo(placemark: CLPlacemark) {
-            //stop updating location to save battery life
-            locationManager.stopUpdatingLocation()
-            guard let locality = placemark.locality else {return}
-            guard let postcode = placemark.postalCode else {return}
-            guard let administrativeArea = placemark.administrativeArea else {return}
-            guard let country = placemark.country else {return}
-            self.adress.text = "Adress: \(locality), \(postcode), \(administrativeArea), \(country)"
+    @IBAction func finishButtonAction(_ sender: Any) {
     }
+    
     
 
 }
