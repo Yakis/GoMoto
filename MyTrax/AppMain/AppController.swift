25f3d0372f5d66725d59e08f65b3882337fd51cb
Mugurel Moscaliuc <yakis@Mugurels-iMac.local>
2017-11-07 19:04:20 +0000

    TabBarController with views is working

--- MyTrax/AppMain/AppController.swift
+++ MyTrax/AppMain/AppController.swift
@@ -17,19 +17,52 @@ class AppController: NSObject {
     }
     
     class func createAndReturnRoot() -> UIViewController {
-//        switch Auth.auth().currentUser {
-//        case nil:
-//            let welcomeViewController = WelcomeVC(nibName: WelcomeNibs.welcomeVC, bundle: nil)
-//            return welcomeViewController
-//        default:
-//            let mainViewController = AditionalInfoVC(nibName: "AditionalInfoVC", bundle: nil)
-//            return mainViewController
-//        }
-        //let dashboardViewController = DashboardVC(nibName: DashboardNibs.dashboardVC, bundle: nil)
-        let welcomeViewController = WelcomeVC(nibName: WelcomeNibs.welcomeVC, bundle: nil)
-       // let navigationController = UINavigationController(rootViewController: welcomeViewController)
-        return welcomeViewController
+        switch Auth.auth().currentUser {
+        case nil:
+            let welcomeViewController = WelcomeVC(nibName: WelcomeNibs.welcomeVC, bundle: nil)
+            return welcomeViewController
+        default:
+            return self.createAndReturnTabBarController()
+        }
     }
+    
+    
+    class func createAndReturnTabBarController() -> UITabBarController {
+        let tabBarController = UITabBarController()
+        
+        let allTracksVC = AllTracksVC(nibName: "AllTracksVC", bundle: nil)
+        let navAllTracks = UINavigationController(rootViewController: allTracksVC)
+        navAllTracks.navigationBar.topItem?.title = "All Tracks"
+        allTracksVC.view.backgroundColor = UIColor.green
+        allTracksVC.tabBarItem = UITabBarItem(title: "Track List", image: #imageLiteral(resourceName: "trackListTabBar"), selectedImage: #imageLiteral(resourceName: "trackListTabBar"))
+        
+        let favoritesVC = UIViewController()
+        let navFavorites = UINavigationController(rootViewController: favoritesVC)
+        favoritesVC.view.backgroundColor = UIColor.red
+        navFavorites.navigationBar.topItem?.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(title: "Favorites", image: #imageLiteral(resourceName: "favoritesTabBar"), selectedImage: #imageLiteral(resourceName: "favoritesTabBar"))
+        
+        let riderProfileVC = UIViewController()
+        let navRiderVC = UINavigationController(rootViewController: riderProfileVC)
+        riderProfileVC.view.backgroundColor = UIColor.yellow
+        riderProfileVC.tabBarItem = UITabBarItem(title: "Rider Profile", image: #imageLiteral(resourceName: "riderProfileTabBar"), selectedImage: #imageLiteral(resourceName: "riderProfileTabBar"))
+        
+        let notificationsVC = UIViewController()
+        let navNotifications = UINavigationController(rootViewController: notificationsVC)
+        notificationsVC.view.backgroundColor = UIColor.brickRed
+        notificationsVC.tabBarItem = UITabBarItem(title: "Notifications", image: #imageLiteral(resourceName: "notificationTabBar"), selectedImage: #imageLiteral(resourceName: "notificationTabBar"))
+        
+        let sellingVC = UIViewController()
+        let navSelling = UINavigationController(rootViewController: sellingVC)
+        sellingVC.view.backgroundColor = UIColor.mediumGray
+        sellingVC.tabBarItem = UITabBarItem(title: "Notifications", image: #imageLiteral(resourceName: "sellTabBar"), selectedImage: #imageLiteral(resourceName: "sellTabBar"))
+        
+        let viewControllers = [navAllTracks, navFavorites, navRiderVC, navNotifications, navSelling]
+        tabBarController.viewControllers = viewControllers
+        return tabBarController
+    }
+    
+    
     
     class func setupFirebase() {
         FirebaseConfiguration.shared.setLoggerLevel(.min)
