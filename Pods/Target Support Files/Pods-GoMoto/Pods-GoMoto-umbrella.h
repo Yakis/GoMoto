b2523689a9538e4e5387fee7a346d85b5ac6c98d
Mugurel Moscaliuc <yakis@Mugurels-iMac.local>
2018-02-06 16:12:57 +0000

    App name changed completely

--- Pods/Target Support Files/Pods-GoMoto/Pods-GoMoto-umbrella.h
+++ Pods/Target Support Files/Pods-GoMoto/Pods-GoMoto-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_MyTraxVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MyTraxVersionString[];
+FOUNDATION_EXPORT double Pods_GoMotoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GoMotoVersionString[];
 
